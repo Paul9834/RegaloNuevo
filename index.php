aceb30458bb78d3ba2e8b74061c4ff0fa0c872ee
Kevin Paul Montealegre Melo <Kmontealegremelo@gmail.com>
2020-02-13 11:04:49 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+        <div class="container">
+
+            <a href="#" class="navbar-brand">
+                <img src="assets/img/poli.png" width="200" height="60">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto text-uppercase">
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#services">VIDEO</a></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header style="color: rgb(0,0,0);">
+        <header class="masthead" style="background-image: url(&quot;assets/img/fondo.png&quot;);filter: blur(0px) brightness(100%);">
+            <div class="container" style="filter: blur(0px) contrast(122%);">
+                <div class="intro-text">
+                    <div class="intro-lead-in"><span></span></div>
+                    <div class="intro-heading text-uppercase"><span class="text-capitalize" data-aos="fade" style="font-family: Montserrat, sans-serif;font-size: 43px;">¡Bienvenido al City Campus!</span></div><a class="btn btn-primary border-secondary pulse animated btn-xl text-uppercase js-scroll-trigger"
+                        role="button" href="#services" style="background-color: #1FB2DE;">CUENTAME MÁS</a></div>
+            </div>
+        </header>
+    </header>
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">video</h2>
+                    <h3 class="text-muted section-subheading"></h3>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-4"><iframe allowfullscreen="" frameborder="0" src="https://www.youtube-nocookie.com/embed/4zPPviMj-NU?autoplay=1&amp;mute=1" width="300" height="200" style="margin-bottom: 37px;margin-top: -9px;"></iframe></div>
+                <div class="col" style="margin-left: 0;">
+                    <button class="btn btn-primary" type="button" style="margin-left: 0px;margin-right: 6px;background-color: rgb(242,209,19)">Excelente</button>
+                    <button class="btn btn-primary" type="button" style="background-color: rgb(32,107,252);">Regular</button>
+                    <button class="btn btn-primary"
+                        type="button" style="margin-left: 6px;background-color: rgb(242,62,62);">Malo</button></div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Formación Por Retos © 2020</span></div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/agency.js"></script>
+</body>
+
+</html>
