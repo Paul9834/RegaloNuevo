777ba563c8df0fedd5785348f7ff7ae18665f433
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-02-13 11:14:48 -0500

    Flag Counter

--- index.php
+++ index.php
@@ -61,6 +61,13 @@
             </div>
         </div>
     </section>
+
+    <div align="center">
+    <a href="https://info.flagcounter.com/NDTI"><img src="https://s11.flagcounter.com/count2/NDTI/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
+    </div>
+
+
+
     <footer>
         <div class="container">
             <div class="row">
